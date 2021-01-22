bac6d0e15743ffbd538ffd48521c4e3a30308995
İlhan Cüvelek <75850897+ilhancuvelek@users.noreply.github.com>
2021-01-22 16:55:31 +0300

    My Dictionary Demo

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] _key;
+        Tvalue[] _value;
+        Tkey[] tempKey;
+        Tvalue[] tempValue;
+
+
+        public MyDictionary()
+        {
+            _key = new Tkey[0];
+            _value = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            tempKey = _key;
+            tempValue = _value;
+
+            _key = new Tkey[_key.Length + 1];
+            _value = new Tvalue[_value.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _key[i] = tempKey[i];
+                _value[i] = tempValue[i];
+            }
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+
+
+
+        public int lenght
+        {
+            get { return _key.Length; }
+
+        }
+
+        public Tkey[] Keys
+        {
+            get { return _key; }
+
+        }
+
+        public Tvalue[] Values
+        {
+            get { return _value; }
+
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDict = new MyDictionary<string, string>();
+            myDict.Add("ilhan", "cüvelek");
+            myDict.Add("eren", "cüvelek");
+            myDict.Add("evren", "cüvelek");
+            Console.WriteLine(myDict.lenght);
+            foreach (string item in myDict.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
